a88f08b86be9c014cd78d70538e7d0e98206a677
Uriel Abraham Lopez Fleischer <abraham2160@hotmail.com>
2022-04-06 10:23:51 -0700

    Avance
    La pág. login entra, falta la configuración del documento para los usuarios , falta que redirija a la pagina principal  y crear usuario.

--- app.py
+++ app.py
@@ -13,6 +13,52 @@ def handle_context():
 def index():
     return render_template("index.html")
 
+@app.route('/login', methods=['GET','POST'])
+@app.route('/login/', methods=['GET','POST'])
+def login():
+    if request.method == 'GET':
+        msg = ''
+        return render_template('login.html',mensaje=msg)
+    #"""
+    else:
+        if request.method == 'POST':
+            #print(request.form.keys())
+            usuario = request.form['usuario']
+            if usuario in diccionario_usuarios:
+                password_db = diccionario_usuarios[usuario]['password'] # password guardado
+                password_forma = request.form['password'] #password presentado
+                verificado = sha256_crypt.verify(password_forma,password_db)
+                if (verificado == True):
+                    session['usuario'] = usuario
+                    session['logged_in'] = True
+                    if 'ruta' in session:
+                        ruta = session['ruta']
+                        session['ruta'] = None
+                        return redirect(ruta)
+                    else:
+                        return redirect("/")
+                else:
+                    msg = f'El password de {usuario} no corresponde'
+                    return render_template('login.html',mensaje=msg)
+            else:
+                msg = f'usuario {usuario} no existe'
+                return render_template('login.html',mensaje=msg)
+    #"""
+
+@app.route('/new_user', methods=['GET','POST'])
+@app.route('/new_user/', methods=['GET','POST'])
+def new_user():
+    if request.method == 'GET':
+        msg = ''
+        return render_template('new_user.html',mensaje=msg)
+
+@app.route('/a_new_user', methods=['GET','POST'])
+@app.route('/a_new_user/', methods=['GET','POST'])
+def a_new_user():
+    if request.method == 'GET':
+        msg = ''
+        return render_template('a_new_user.html',mensaje=msg)
+
 @app.route('/logout', methods=['GET'])
 @app.route('/logout/', methods=['GET'])
 def logout():
