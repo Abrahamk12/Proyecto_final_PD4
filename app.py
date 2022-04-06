708b9c2aa032a28249b199f5166eb7ec7bb5c85a
Uriel Abraham Lopez Fleischer <abraham2160@hotmail.com>
2022-04-06 14:32:44 -0700

    Avance
    Da error al intentar iniciar sesión, faltan mas cosas por hacer :V

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, redirect, render_template, request, session
-from funciones import compara_distancia, crea_diccionario, crea_superdiccionario, graba_diccionario, lee_diccionario_csv, lee_diccionario_peliculas, crea_diccionario_peliculas, crea_diccionario_generos, crea_diccionario_x_anio
+from funciones import graba_diccionario, lee_diccionario_csv
 from passlib.hash import sha256_crypt
 import os
 
--- funciones.py
+++ funciones.py
@@ -1,8 +1,60 @@
 import csv
 from passlib.hash import sha256_crypt
 from Levenshtein import distance
+#'''
+class Usuarios():
+    def __init__(self,id,user,pasword):
+        self.id = id
+        self.user = user
+        self.pasword = pasword
+    
+    #def __str__(self):
+    #    return f'{self.user} - {self.pasword}'
 
-"""
-Corregir esta parte para que lea el archivo:
-Para qeu lea el archivo usuario, y los sig como si fuera una base de datos
-"""
+def lee_diccionario_csv(archivo:str)->list:
+    #Lee un archivo CSV y regresa un diccionario de diccionarios
+    diccionario = {}
+    try:
+        with open(archivo,'r',encoding='utf-8') as fh:
+            csv_reader = csv.DictReader(fh)
+            for renglon in csv_reader:
+                llave = renglon['usuario']
+                diccionario[llave]=renglon
+    except IOError:
+        print(f"No se pudo leer el archivo {archivo}")
+    return diccionario
+
+def lee_archivo_csv(archivo:str)->list:
+    #Lee un archivo CSV y regresa una lista de registros
+    lista = []
+    try:
+        with open(archivo,'r',encoding='utf-8') as fh:
+            csv_reader = csv.reader(fh)
+            for renglon in csv_reader:
+                lista.append(renglon)
+    except IOError:
+        print(f"No se pudo leer el archivo {archivo}")
+    return lista
+
+def obten_campos(diccionario:dict,llave_d:str)->list:
+    lista = [llave_d]
+    llaves = list(diccionario.keys())
+    k = llaves[0]
+    nuevo_diccionario = diccionario[k]
+    lista_campos = list(nuevo_diccionario.keys())
+    lista.extend(lista_campos)
+    return lista
+
+def graba_diccionario(diccionario:dict,llave_dict:str,archivo:str):
+    with open(archivo,'w') as fh: #fh = file handle
+        lista_campos = obten_campos(diccionario, llave_dict)
+        dw = csv.DictWriter(fh,lista_campos)
+        dw.writeheader()
+        renglones = []
+        for llave, valor_d in diccionario.items():
+            d = { 'usuario':llave}
+            for key, value  in valor_d.items():
+                d[key] = value
+            renglones.append(d)
+        dw.writerows(renglones)
+#'''
